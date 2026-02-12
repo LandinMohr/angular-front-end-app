459a36c27a58e17007c6e6c9dc8ddcab562dc6ad
4022s <4022s@users.noreply.local>
2026-02-12 11:09:52 -0800

    made thingys :)

---
+++ src/services/api.service.ts
@@ -0,0 +1,22 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable, signal} from '@angular/core';
+import {APIResponse} from '../models/api.object';
+
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+private base = 'https://api.restful-api.dev/objects';
+readonly loading =signal(false);
+readonly error =signal<string | null>(null);
+
+constructor(private http: HttpClient) {}
+
+getObjects() {
+    this.loading.set(true);
+return this.http.get<APIResponse[]>(this.base);
+}
+
+getObject(id: string) {
+    this.loading.set(true);
+return this.http.get<APIResponse>(`${this.base}/${id}`);
+}
+}
